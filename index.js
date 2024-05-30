931875a7bce139af0e58f8f9f2d555287cadd7c9
kooharim <166689288+koohrim12@users.noreply.github.com>
2024-05-30 14:38:06 +0900

    inde.js-뼈대 태그

---
+++ index.js
@@ -0,0 +1,23 @@
+root.appendChild(div);
+
+function imgTag() {
+  for (let i = 0; i < 2; i++) {
+    const img = document.createElement("img");
+
+    // console.log(img);
+
+    div.appendChild(img);
+  }
+}
+imgTag();
+const h1 = document.createElement("h1");
+
+// console.log(h1);
+
+const button = document.createElement("button");
+
+// console.log(button);
+
+div.appendChild(h1);
+
+div.appendChild(button);
