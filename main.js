91ca22987507d860e37c872606281d0a428d42f6
kooharim <166689288+koohrim12@users.noreply.github.com>
2024-05-31 11:38:00 +0900

    버튼 태그 생성

--- main.js
+++ main.js
@@ -3,3 +3,6 @@ console.log(root);
 const div = document.createElement("div");
 root.appendChild(div);
 console.log(div);
+const button = document.createElement("button");
+root.appendChild(button);
+console.log(button);
