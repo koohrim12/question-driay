7fa05608ad745ef95b817ca3d725cb4dc734e5e0
kooharim <166689288+koohrim12@users.noreply.github.com>
2024-05-31 12:34:06 +0900

    질문 더 받기 버튼을 누르면 질문 div생성

--- main.js
+++ main.js
@@ -6,3 +6,36 @@ console.log(div);
 const button = document.createElement("button");
 root.appendChild(button);
 console.log(button);
+// const input = document.createElement("input");
+// root.appendChild(input);
+// console.log(input);
+
+// root -style
+root.style.width = "500px";
+root.style.height = "500px";
+root.style.backgroundColor = "pink";
+root.style.display = "flex";
+root.style.justifyContent = "center";
+root.style.alignContent = "center";
+root.style.flexDirection = "column-reverse";
+
+// 버튼 - style
+button.innerHTML = "전송";
+button.style.width = "100px";
+button.style.height = "50px";
+button.style.borderRadius = "30px";
+
+// input - style
+// input.style.width = "300px";
+// input.style.height = "300px";
+
+function textAppear() {
+  let clickBtn = document.createElement("div");
+  clickBtn.innerHTML = "이건 클릭 했을 때 나오는 텍스트이다.";
+  root.appendChild(clickBtn);
+  clickBtn.style.width = "200px";
+  clickBtn.style.height = "50px";
+  clickBtn.style.backgroundColor = "gray";
+  clickBtn.style.margin = "5px";
+}
+button.addEventListener("click", textAppear);
