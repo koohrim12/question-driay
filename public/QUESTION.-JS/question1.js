18cebb39d11e0b6f6aadaf69195d12582fd471de
kooharim <166689288+koohrim12@users.noreply.github.com>
2024-06-28 13:04:08 +0900

    style_ 구하림: js로 동적으로 div1추가와 크기 지정

--- public/QUESTION.-JS/question1.js
+++ public/QUESTION.-JS/question1.js
@@ -1,4 +1,18 @@
 const root = document.getElementById("root");
 console.log(root);
-const div = document.createElement(div);
-root.app;
+const div1 = document.createElement("div1");
+root.appendChild(div1);
+console.log(div1);
+
+// root -  style
+root.style.width = "100%";
+root.style.height = "100vh";
+root.style.backgroundColor = "black";
+root.position = "relative";
+
+// div1 - style
+
+div1.style.width = "500px";
+div1.style.height = "500px";
+div1.style.position = "absoute";
+div1.style.backgroundColor = "red";
